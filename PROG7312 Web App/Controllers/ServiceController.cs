9345df16588ae7333ef3bb2db8fb06bbff9a8f13
Dean James <deanjamesgreeff@gmail.com>
2025-11-11 16:29:08 +0200

    Update/UI-Styling
    Changed:
    - CSS styling
    - HTML element's Bootstrap

---
+++ PROG7312 Web App/Controllers/ServiceController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PROG7312_Web_App.Controllers
+{
+    public class ServiceController : Controller
+    {
+
+        public IActionResult ServicePartial()
+        {
+            return PartialView("_Service");
+        }
+
+        public IActionResult RequestServicePartial()
+        {
+            return PartialView("_RequestService");
+        }
+    }
+}
